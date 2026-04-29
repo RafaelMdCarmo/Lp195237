66a79e7bdc3a4dbbdf6b401e9beeece9c52926ef
RafaelMdCarmo <rafael.mello@unifesp.br>
2026-04-29 08:31:41 -0300

    Update 1000.C

--- 1000.C
+++ 1000.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Rafael Mello do Carmo
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 29/04/2026
+Objetivo    : mostrar texto em tela
+Aprendizado : estrutura basica de um codigo
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
  
 int main()
