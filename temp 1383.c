9363b6e874879f24d022c4857885500bd8aa7da2
RafaelMdCarmo <rafael.mello@unifesp.br>
2026-06-16 16:52:13 -0300

    Update temp 1383.c

--- temp 1383.c
+++ temp 1383.c
@@ -1,45 +1,66 @@
 #include <stdio.h>
 
+
 int main()
 {
-	int sudoku[3][3];
+	int sudoku[9][9];
+	
+	//int celulas[3][3];
+
 	int resposta = 1;
+	int num = 0;
 
-	for(int i = 0; i < 3; i++) {
-		for(int j = 0; j < 3; j++) {
-			scanf("%i", &sudoku[i][j]);
+	scanf("%i", &num);
+
+    //linha e coluna
+    
+	for(int n = 0; n < num; n++) {
+
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j < 9; j++) {
+				scanf("%i", &sudoku[i][j]);
+			}
 		}
-	}
 
-	for(int i = 0; i < 3; i++) {
-		for(int j = 0; j < 3; j++) {
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j < 9; j++) {
 
-			for(int x = 0; x < 3; x++) {
-				if(i != x && sudoku[i][j] == sudoku[x][j]) {
-					resposta = 0;
+				for(int x = 0; x < 9; x++) {
+					if(i != x && sudoku[i][j] == sudoku[x][j]) {
+						resposta = 0;
+					}
 				}
 			}
 		}
-	}
 
-	for(int i = 0; i < 3; i++) {
-		for(int j = 0; j < 3; j++) {
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j < 9; j++) {
 
-			for(int y = 0; y < 3; y++) {
-				if(j != y && sudoku[i][j] == sudoku[i][y]) {
-					resposta = 0;
+				for(int y = 0; y < 9; y++) {
+					if(j != y && sudoku[i][j] == sudoku[i][y]) {
+						resposta = 0;
+					}
 				}
 			}
 		}
-	}
+
+    //celula
+    //printf("%i", celulas[3][3]);
+    
+    
 
 
 
-	if (resposta == 1) {
-		printf("SIM");
-	}
-	else {
-		printf("NAO");
+
+        printf("Instancia %i\n", n+1);
+        
+		if (resposta == 1) {
+			printf("SIM\n\n");
+		}
+		else {
+			printf("NAO\n\n");
+		}
+
 	}
 
 	return 0;
